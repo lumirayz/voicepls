2c8430f86206435c879c659e597e8514b00eecee
lumirayz <lumirayz@gmail.com>
2013-12-04 16:28:58 +0100

    added muji and jingle namespaces

--- xmlns.js
+++ xmlns.js
@@ -31,3 +31,5 @@ exports.client     = "jabber:client";
 exports.muc        = "http://jabber.org/protocol/muc";
 exports.muc_user   = exports.muc + "#user";
 exports.chatstates = "http://jabber.org/protocol/chatstates";
+exports.muji       = "http://telepathy.freedesktop.org/muji";
+exports.jingle     = "urn:xmpp:jingle:1";
